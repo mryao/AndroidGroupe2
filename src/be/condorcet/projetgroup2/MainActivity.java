8a38d871842c46c9c923e7f9b3a0ee429a2b03e9
amory.kievits <amorykievits@hotmail.com>
2014-11-26 09:31:12 +0100

    a l'aise

--- src/be/condorcet/projetgroup2/MainActivity.java
+++ src/be/condorcet/projetgroup2/MainActivity.java
@@ -1,17 +1,39 @@
 package be.condorcet.projetgroup2;
 
+
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
+	
+	private Button suivant =null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//test15665
+		
+		suivant=(Button)findViewById(R.id.ok);
+		
+		suivant.setOnClickListener(
+				new OnClickListener(){
+					
+					public void onClick(View v){
+						
+						Intent i = new Intent(MainActivity.this,CreerTache.class);
+						startActivity(i);
+					
+					}//fin méthode onClick
+				  }//fin classe onClickLister
+				);//fin paramétrage méthode setOnClickListener
+		
 	}
 
 	@Override
