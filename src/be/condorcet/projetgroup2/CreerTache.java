4a98dc78b51f1de7cc66975d72c4de0fd2520c31
Arc <gracithomas@hotmail.com>
2014-11-26 09:58:38 +0100

    voilà

--- src/be/condorcet/projetgroup2/CreerTache.java
+++ src/be/condorcet/projetgroup2/CreerTache.java
@@ -1,12 +1,20 @@
 package be.condorcet.projetgroup2;
 
+import java.sql.Connection;
+
+import classdb.TacheDB;
+import Connexion.DBConnection;
 import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class CreerTache extends ActionBarActivity {
 
+	private Connection con=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,4 +39,82 @@ public class CreerTache extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		 try {
+	          con.close();
+	          con=null;
+	          Log.d("connexion","deconnexion OK");
+	          }
+	          catch (Exception e) { 
+	          }
+		 Log.d("connexion","deconnexion OK");
+		
+	}
+	
+	 class MyAccesDB extends AsyncTask<String,Integer,Boolean> {
+		    private String resultat;
+		    private ProgressDialog pgd=null;
+		    
+								
+					public MyAccesDB(CreerTache pActivity) {
+					
+						link(pActivity);
+						// TODO Auto-generated constructor stub
+					}
+
+					private void link(CreerTache pActivity) {
+						// TODO Auto-generated method stub
+					
+						
+					}
+					//se fait avant l'opération
+					protected void onPreExecute(){
+						 super.onPreExecute();
+				         pgd=new ProgressDialog(CreerTache.this);
+						 pgd.setMessage("chargement en cours");
+						 pgd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			     		 pgd.show();
+													
+					}
+					//se fait pendant l'opération
+					@Override
+					protected Boolean doInBackground(String... arg0) {
+											
+					   if(con==null){//premier invocation
+						   con = new DBConnection().getConnection(); 
+					    	if(con==null) {
+						    resultat="echec de la connexion";
+					      	return false;
+						    }
+					  
+						   TacheDB.setConnection(con);
+					   }
+					    int id=Integer.parseInt(ed1.getText().toString());	
+				        try{
+				        	
+				        	TacheDB tc=new TacheDB(id);	
+				           	       
+				          		           
+				        }
+				        catch(Exception e){		             
+				         resultat="erreur" +e.getMessage(); 
+				         return false;
+				         
+				         }
+				               
+					
+						return true;
+					}
+					//se fait après l'opération, impossible de modifier des valeurs
+					protected void onPostExecute(Boolean result){
+						 super.onPostExecute(result);
+						  pgd.dismiss();
+						  ed2.setText(resultat);
+									
+					}
+			
+				}
 }
