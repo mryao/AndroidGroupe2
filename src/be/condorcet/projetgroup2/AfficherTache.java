02d0512310d4e2660aa4b3ce06f340f1d5094633
amory.kievits <amorykievits@hotmail.com>
2014-12-12 16:49:34 +0100

    juste afficher tache et layout

---
+++ src/be/condorcet/projetgroup2/AfficherTache.java
@@ -0,0 +1,15 @@
+package be.condorcet.projetgroup2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AfficherTache extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_afficher_tache);
+	}
+}
